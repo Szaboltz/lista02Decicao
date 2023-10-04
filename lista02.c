fde8dd62ecf2ca4f3639253d0c6f49f6a87b710c
Henrique Szabo <118364512+Szaboltz@users.noreply.github.com>
2023-10-04 15:54:52 -0300

    Add files via upload

--- lista02.c
+++ lista02.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 void q1() {
 	int n1, n2, n3, tempMaior, tempMedio, tempMenor, r;
@@ -109,7 +111,80 @@ void q4() {
 }
 
 void q5() {
-	
+	float p;
+
+	printf("Digite o indice de poluicao atual: ");
+    scanf("%f", &p);
+
+    if (p >= 0.3 && p < 0.4) {
+        printf("A empresa do primeiro grupo devera ser notificada");
+    } else if (p >= 0.4 && p < 0.5) {
+        printf("A empresa do primeiro e segundo grupo deverao ser notificadas");
+    } else if (p >= 0.5) {
+        printf("Todas as empresas serao notificadas");
+    } else {
+        printf("Siga o trabalho normal");
+    }
+}
+
+void q6() {
+	int ano;
+
+	printf("Digite o ano para saber se e bissexto: ");
+	scanf("%d", &ano);
+
+	if (ano >= 1581) {
+		if (ano % 4 == 0) {
+			if (ano % 100 == 0) {
+				if (ano % 400 == 0) {
+					printf("%d e bissexto", ano);
+				} else {
+					printf("%d nao e bissexto", ano);
+				}
+			} else {
+				printf("%d e bissexto", ano);
+			}
+		} else {
+			printf("%d nao e bissexto", ano);
+		}
+	} else {
+		printf("Ano invalido");
+	}
+} 
+
+int num_pseudo(){
+    srand(time(NULL));
+    return (rand() % 7); //retorna um numero pseudo-aleatório de 0 a 6
+}
+
+void q7() {
+	int n = num_pseudo();
+
+	switch (n)
+	{
+	case 0:
+		printf("segunda-feira");
+		break;
+	case 1:
+		printf("terca-feira");
+		break;
+	case 2:
+		printf("quarta-feira");
+		break;
+	case 3:
+		printf("quinta-feira");
+		break;
+	case 4:
+		printf("sexta-feira");
+		break;
+	case 5:
+		printf("sabado");
+		break;
+	case 6:
+		printf("domingo");
+		break;
+	}
+
 }
 
 int main () {
@@ -120,6 +195,12 @@ int main () {
 	//q3();
 	//printf("\n");
 	//q4();
+	//printf("\n");]
+	//q5();
+	//printf("\n");
+	//q6();
+	//printf("\n");
+	//q7();
 	//printf("\n");
 	
 	return 0;
